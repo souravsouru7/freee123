f1f1375bd1dccbfd996bc6236240c577e9a2c22d
sourav <soutavr5@gmail.com>
2025-04-28 13:07:35 +0530

    grhwr

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -47,7 +47,7 @@ const Footer = () => {
                 transition={{ type: 'spring', stiffness: 300 }}
               >
                 <i className="fas fa-map-marker-alt"></i>
-                <span>Dubai, UAE</span>
+                <span> Ras Al Khaimah,UAE</span>
               </motion.li>
               <motion.li
                 whileHover={{ x: 10 }}
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -294,7 +294,7 @@ const Hero = () => {
                   animate={{ scale: 1, opacity: 1 }}
                   transition={{ delay: 1.6, type: 'spring', stiffness: 100 }}
                 >
-                  Ras Al Khaimah, UAE
+                  Ras Al Khaimah,UAE
                 </motion.div>
 
                 <motion.div
