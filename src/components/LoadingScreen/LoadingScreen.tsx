593ae42130ccb82aa4c96c97e054cd7c34049d96
sourav <soutavr5@gmail.com>
2025-04-28 10:24:24 +0530

    ehrer

--- src/components/LoadingScreen/LoadingScreen.tsx
+++ src/components/LoadingScreen/LoadingScreen.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './LoadingScreen.scss';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo2.png';
 
 const LoadingScreen: React.FC = () => {
   const [isVisible, setIsVisible] = useState(true);
